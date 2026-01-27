25f2df9aa619d76f094ed256bc91fdb1ac19b43f
Stan-breaks <stanleymwendwa03@gmail.com>
2026-01-27 21:58:06 +0300

    cleanup: remove old instructor_home folder and relocated student files
    - Remove deprecated instructor_home folder
    - Delete old explore_page.dart location
    - Delete old profile_page.dart location

--- lib/screens/instructor_home/instructor_dashboard.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class InstructorDashboard extends StatelessWidget {
-  const InstructorDashboard({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}

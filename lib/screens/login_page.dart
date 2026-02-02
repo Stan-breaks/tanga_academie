7d1be7a263c464e60bf5beb7bdc56b0b9153aed4
Stan-breaks <stanleymwendwa03@gmail.com>
2026-02-03 00:12:36 +0300

    style(auth): minor adjustments to login and signup pages

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -25,6 +25,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -32,6 +32,7 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
